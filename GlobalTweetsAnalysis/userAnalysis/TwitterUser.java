0a91b72fa028e3d99d13cb38f5e4ee765605f2d0
Yi <andrew.hanyi@gmail.com>
2014-05-23 15:43:49 +1000

    Create TwitterUser.java

---
+++ GlobalTweetsAnalysis/userAnalysis/TwitterUser.java
@@ -0,0 +1,13 @@
+package userAnalysis;
+
+public class TwitterUser {
+	
+	public long userId;
+	//public String userDescription;
+	public int scoreDescription;
+	public int friendsCount;
+	public int followersCount;
+	public int tweetsCount;
+	public int positiveTweetsCount;
+	public int negativeTweetsCount;
+}
